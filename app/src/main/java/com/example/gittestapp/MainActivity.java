6404efb6868b58331db58cf127c48fb54a85f3ac
Abdullah Razzaq <abdullahrazzaq120@gmail.com>
2021-07-08 16:09:59 +0500

    c4 of b1

--- app/src/main/java/com/example/gittestapp/MainActivity.java
+++ app/src/main/java/com/example/gittestapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gittestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,14 +11,23 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button btn1;
+    ProgressDialog loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loader = new ProgressDialog(MainActivity.this);
 
         btn1 = findViewById(R.id.btn1);
-        btn1.setOnClickListener(view ->
-                Toast.makeText(MainActivity.this, "Button is being clicked!", Toast.LENGTH_SHORT).show());
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button is being clicked!", Toast.LENGTH_SHORT).show();
+                loader.setTitle("Loading...");
+                loader.show();
+                loader.setCanceledOnTouchOutside(false);
+            }
+        });
     }
 }
